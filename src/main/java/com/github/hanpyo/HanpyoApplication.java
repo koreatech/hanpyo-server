76bf3fea63356d2ba03ac6acdbd48530bac675fe
youngxpepp <youngxpepp@gmail.com>
2021-03-23 11:07:16 +0900

    chore: 저장소 초기화

---
+++ src/main/java/com/github/hanpyo/HanpyoApplication.java
@@ -0,0 +1,12 @@
+package com.github.hanpyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanpyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanpyoApplication.class, args);
+	}
+}
