7813de77c698a77dcf4a8f9b6b7dd1412a024d52
youngxpepp <youngxpepp@gmail.com>
2021-04-08 14:58:13 +0900

    feat: 시큐리티 메소드 기반 설정 추가

--- src/main/java/com/github/hanpyo/config/security/SecurityConfig.java
+++ src/main/java/com/github/hanpyo/config/security/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
