d7f9c26c375fb7802d22ebcb8e4c252cbac64bc4
Do-ho <hong7511059@koreatech.ac.kr>
2021-04-13 13:58:21 +0900

    feat: LectureTimeRepository 생성

---
+++ src/main/java/com/github/hanpyo/repository/LectureTimeRepository.java
@@ -0,0 +1,7 @@
+package com.github.hanpyo.repository;
+
+import com.github.hanpyo.entity.LectureTime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LectureTimeRepository extends JpaRepository<LectureTime, Long> {
+}
