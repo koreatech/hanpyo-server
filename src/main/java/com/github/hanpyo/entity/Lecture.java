4c9e16f8421cc329b37d3d287ec7d3f3bbfd75bd
Do-ho <hong7511059@koreatech.ac.kr>
2021-04-13 13:55:06 +0900

    refactor: Lecture 생성자 코드 수정
    - lectureInfo를 통해 비즈니스 로직을 따로 관리하는 코드 작성

--- src/main/java/com/github/hanpyo/entity/Lecture.java
+++ src/main/java/com/github/hanpyo/entity/Lecture.java
@@ -20,60 +20,94 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Lecture extends AbstractBaseTimeEntity {
 
-	@Id
-	@Column(name = "lecture_id")
-	private Long id;
+    @Id
+    @Column(name = "lecture_id")
+    private Long id;
 
-	@Column(name = "lecture_code")
-	private String code;
+    @Column(name = "lecture_code")
+    private String code;
 
-	@Column(name = "lecture_name")
-	private String name;
+    @Column(name = "lecture_name")
+    private String name;
 
-	@Column(name = "lecture_room")
-	private String room;
+    @Column(name = "lecture_room")
+    private String room;
 
-	@Column(name = "lecture_professor")
-	private String professor;
+    @Column(name = "lecture_professor")
+    private String professor;
 
-	@Column(name = "lecture_credit")
-	private Integer credit;
+    @Column(name = "lecture_credit")
+    private Integer credit;
 
-	@Column(name = "lecture_required_grade")
-	private Integer requiredGrade;
+    @Column(name = "lecture_required_grade")
+    private Integer requiredGrade;
 
-	@Column(name = "lecture_required_major")
-	private String requiredMajor;
+    @Column(name = "lecture_required_major")
+    private String requiredMajor;
 
-	@Column(name = "lecture_total_student_number")
-	private Integer totalStudentNumber;
+    @Column(name = "lecture_total_student_number")
+    private Integer totalStudentNumber;
 
-	@Column(name = "lecture_current_student_number")
-	private Integer currentStudentNumber;
+    @Column(name = "lecture_current_student_number")
+    private Integer currentStudentNumber;
 
-	@Column(name = "lecture_division_number")
-	private Integer divisionNumber;
+    @Column(name = "lecture_division_number")
+    private Integer divisionNumber;
 
-	@Column(name = "lecture_department")
-	private String department;
+    @Column(name = "lecture_department")
+    private String department;
 
-	@OneToMany(mappedBy = "lecture")
-	private List<LectureTime> lectureTimes = new ArrayList<>();
+    @OneToMany(mappedBy = "lecture")
+    private List<LectureTime> lectureTimes = new ArrayList<>();
 
-	@Builder
-	public Lecture(Map<String, Object> lectureInfo) {
-		this.id = Long.parseLong((String) lectureInfo.get("LECT_NO"));
-		this.code = (String) lectureInfo.get("CORS_CD");
-		this.name = (String) lectureInfo.get("CORS_NM");
-		this.room = (String) lectureInfo.get("LECT_RM");
-		this.professor = (String) lectureInfo.get("PROF_NM");
-		this.credit = Integer.parseInt((String) lectureInfo.get("CREDIT"));
-		this.requiredGrade = Integer.parseInt((String) lectureInfo.get("SCH_YR"));
-		this.requiredMajor = "";
-		if (lectureInfo.get("CLS_CNT") == null) this.totalStudentNumber = 0;
-		else this.totalStudentNumber = Integer.parseInt((String) lectureInfo.get("CLS_CNT"));
-		this.currentStudentNumber = Integer.parseInt((String) lectureInfo.get("LECT_CNT"));
-		this.divisionNumber = Integer.parseInt((String) lectureInfo.get("CLS_NO"));
-		this.department = (String) lectureInfo.get("DEPT_NM");
-	}
+    @Builder
+    public Lecture(Long id, String code, String name, String room, String professor, Integer credit, Integer requiredGrade, String requiredMajor, Integer totalStudentNumber, Integer currentStudentNumber, Integer divisionNumber, String department, List<LectureTime> lectureTimes) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.room = room;
+        this.professor = professor;
+        this.credit = credit;
+        this.requiredGrade = requiredGrade;
+        this.requiredMajor = requiredMajor;
+        this.totalStudentNumber = totalStudentNumber;
+        this.currentStudentNumber = currentStudentNumber;
+        this.divisionNumber = divisionNumber;
+        this.department = department;
+        this.lectureTimes = lectureTimes;
+    }
+
+    public static Lecture from(Map<String, Object> lectureInfo) {
+        Long id = Long.parseLong((String) lectureInfo.get("LECT_NO"));
+        String code = (String) lectureInfo.get("CORS_CD");
+        String name = (String) lectureInfo.get("CORS_NM");
+        String room = "";
+        if (lectureInfo.get("LECT_RM") != null)
+            room = (String) lectureInfo.get("LECT_RM");
+        String professor = (String) lectureInfo.get("PROF_NM");
+        Integer credit = Integer.parseInt((String) lectureInfo.get("CREDIT"));
+        Integer requiredGrade = Integer.parseInt((String) lectureInfo.get("SCH_YR"));
+        String requiredMajor = "";
+        Integer totalStudentNumber = 0;
+        if (lectureInfo.get("CLS_CNT") != null)
+            totalStudentNumber = Integer.parseInt((String) lectureInfo.get("CLS_CNT"));
+        Integer currentStudentNumber = Integer.parseInt((String) lectureInfo.get("LECT_CNT"));
+        Integer divisionNumber = Integer.parseInt((String) lectureInfo.get("CLS_NO"));
+        String department = (String) lectureInfo.get("DEPT_NM");
+
+        return Lecture.builder()
+                .id(id)
+                .code(code)
+                .name(name)
+                .room(room)
+                .professor(professor)
+                .credit(credit)
+                .requiredGrade(requiredGrade)
+                .requiredMajor(requiredMajor)
+                .totalStudentNumber(totalStudentNumber)
+                .currentStudentNumber(currentStudentNumber)
+                .divisionNumber(divisionNumber)
+                .department(department)
+                .build();
+    }
 }
