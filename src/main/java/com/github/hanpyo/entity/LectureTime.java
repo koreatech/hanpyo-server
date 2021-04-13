eb465020b5404d89cefe119bea4d587b874e5193
Do-ho <hong7511059@koreatech.ac.kr>
2021-04-13 13:57:21 +0900

    feat: LectureTime Builder 및 비즈니스 로직 작성

--- src/main/java/com/github/hanpyo/entity/LectureTime.java
+++ src/main/java/com/github/hanpyo/entity/LectureTime.java
@@ -10,9 +10,14 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.google.common.base.Objects;
+
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.Map;
 
 @Entity
 @Table(name = "lecture_time")
@@ -34,4 +39,49 @@ public class LectureTime {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "lecture_id")
 	private Lecture lecture;
+
+	@Builder
+	public LectureTime(Integer startTime, Integer endTime, Lecture lecture) {
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.lecture = lecture;
+	}
+
+	public static LectureTime from (String time, Lecture lecture) {
+		Map<String, Integer> days = Map.of(
+				"월", 540,
+				"화", 1980,
+				"수", 3420,
+				"목", 4860,
+				"금", 6300,
+				"토", 7740,
+				"일", 9180
+		);
+
+		Map<String, Integer> alphas = Map.of(
+				"A", 0,
+				"B", 30
+		);
+
+		Integer dayTime = days.get(time.substring(0, 1));
+		Integer sTime = (Integer.parseInt(time.substring(1, 3)) - 1) * 60 + alphas.get(time.substring(3, 4));
+		Integer eTime = (Integer.parseInt(time.substring(5, 7)) -1) * 60 + alphas.get(time.substring(7, 8)) + 30;
+
+		Integer startTime = dayTime + sTime;
+		Integer endTime = dayTime + eTime;
+
+		return LectureTime.builder()
+				.startTime(startTime)
+				.endTime(endTime)
+				.lecture(lecture)
+				.build();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		LectureTime that = (LectureTime) o;
+		return Objects.equal(id, that.id) && Objects.equal(startTime, that.startTime) && Objects.equal(endTime, that.endTime) && Objects.equal(lecture, that.lecture);
+	}
 }
